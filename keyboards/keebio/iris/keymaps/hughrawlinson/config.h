aae155e9f0c21987405a8d80be95f2c0b5f2b35a
Hugh Rawlinson <hughr2005@gmail.com>
2022-03-31 18:03:38 +0200

    Add my iris config with support for jumped 2nd row

---
+++ keyboards/keebio/iris/keymaps/hughrawlinson/config.h
@@ -0,0 +1,29 @@
+/*
+Copyright 2017 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// #define USE_I2C
+#define EE_HANDS
+
+// for the left hand, bridge the 2nd row to B7 because E6 is blown
+// on the elite-c
+// #ifdef MATRIX_ROW_PINS
+// #undef MATRIX_ROW_PINS
+// #endif
+
+// #define MATRIX_ROW_PINS { D7, B7, B4, D2, D4 }
